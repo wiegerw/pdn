68edcecefd2813e2e2e0b67a9b57de5a47ab732b
“Wieger <j.w.wesselink@tue.nl>
2025-01-09 17:25:18 +0100

    Added a GitHub workflow

--- doc/conf.py
+++ doc/conf.py
@@ -6,10 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Portable Draughts Notation (PDN) Standard'
-copyright = '2025, Wieger Wesselink'
-author = 'Wieger Wesselink'
+project = u'Portable Draughts Notation (PDN) Standard'
+copyright = u'2011 - 2025, Wieger Wesselink'
+author = u'Wieger Wesselink'
 release = '3.0'
+version = '3.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,10 +20,24 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+
+# -- Options for LaTeX output --------------------------------------------------
+
+# The paper size ('letter' or 'a4').
+latex_paper_size = 'a4'
+
+# The font size ('10pt', '11pt' or '12pt').
+latex_font_size = '10pt'
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'pdn_standard.tex', u'PDN 3.0 Standard',
+   u'Wieger Wesselink', 'manual'),
+]
+
